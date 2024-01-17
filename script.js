56402e320163fcae9b5663b79c671a43b16f215f
LeonardoCapocci <leocap2001@gmail.com>
2024-01-17 14:49:07 -0800

    created function to add books to array and started loop to display on page

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
+const myLibrary = [];
+
 function Book(title, author, completion) {
-  this.title = title
-  this.author = author
-  this.completion = completion
-  this.info = function() {
-    console.log(this.title + " is a nice book by " + author + " and I completed it")
+  this.title = title;
+  this.author = author;
+  this.completion = completion;
   }
+
+function addBookToLibrary(title, author, completion) {
+  const newBook = new Book(title, author, completion);
+  myLibrary.push(newBook);
 }
 
-const TNotW = new Book("The Name of the Wind", "Dun Remember", "Completed")
-TNotW.info()
+addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
+
+console.log(myLibrary);
+
+for (let i = 0; i < myLibrary.length; i++) {
+  // Create new div element to contain all the book information.
+  const bookContainer = document.createElement('div');
+  bookContainer.className = 'bookContainer';
+
+  // Title
+  const bookTitle = document.createElement('p');
+  bookTitle.textContent = myLibrary[i].title;
+  bookContainer.appendChild(bookTitle)
+
+  // Append bookContainer to bookList div in body
+  let bookList = document.querySelector('.book-list');
+  bookList.appendChild(bookContainer);
+}
