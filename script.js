7c473af8f32a1e0fcf05b66ae65fac6c83c3ed47
LeonardoCapocci <leocap2001@gmail.com>
2024-01-18 10:58:40 -0800

    completed book cards, added styling, added  header

--- script.js
+++ script.js
@@ -12,18 +12,37 @@ function addBookToLibrary(title, author, completion) {
 }
 
 addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
+addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
+addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
+addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
+addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
+addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
+addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
+addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
 
 console.log(myLibrary);
 
 for (let i = 0; i < myLibrary.length; i++) {
   // Create new div element to contain all the book information.
   const bookContainer = document.createElement('div');
-  bookContainer.className = 'bookContainer';
+  bookContainer.className = 'book-container';
 
   // Title
   const bookTitle = document.createElement('p');
+  bookTitle.className = 'book-title'
   bookTitle.textContent = myLibrary[i].title;
-  bookContainer.appendChild(bookTitle)
+  bookContainer.appendChild(bookTitle);
+
+  // Author
+  const bookAuthor = document.createElement('p');
+  bookAuthor.className = 'book-author'
+  bookAuthor.textContent = myLibrary[i].author;
+  bookContainer.appendChild(bookAuthor);
+
+  // Completion
+  const bookCompletion = document.createElement('p');
+  bookCompletion.textContent = myLibrary[i].completion
+  bookContainer.appendChild(bookCompletion)
 
   // Append bookContainer to bookList div in body
   let bookList = document.querySelector('.book-list');
