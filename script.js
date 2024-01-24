2edc56a7e93a8c0c12f31dcd41a42b23eb54d9fc
LeonardoCapocci <leocap2001@gmail.com>
2024-01-24 14:40:04 -0800

    rewrote code and now books are addable

--- script.js
+++ script.js
@@ -1,59 +1,81 @@
-const myLibrary = [];
+// Empty Book Library
+myLibrary = [];
 
-function Book(title, author, completion) {
+// Book Constructor
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
-  this.completion = completion;
-  }
-
-function addBookToLibrary(title, author, completion) {
-  const newBook = new Book(title, author, completion);
-  myLibrary.push(newBook);
+  this.pages = pages;
+  this.read = read;
 }
 
-addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
-addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
-addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
-addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
-addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
-addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
-addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
-addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", "Completed");
+// Function to add books to the library
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+  updateScreen();
+}
+addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", 800, true);
 
-console.log(myLibrary);
-
-for (let i = 0; i < myLibrary.length; i++) {
-  // Create new div element to contain all the book information.
+// Update Screen
+function updateScreen() {
   const bookContainer = document.createElement('div');
-  bookContainer.className = 'book-container';
+  bookContainer.className = 'book-container'
 
   // Title
   const bookTitle = document.createElement('p');
-  bookTitle.className = 'book-title'
-  bookTitle.textContent = myLibrary[i].title;
-  bookContainer.appendChild(bookTitle);
+  bookTitle.className = 'book-title';
+  bookTitle.textContent = myLibrary[myLibrary.length -1].title;
+  bookContainer.appendChild(bookTitle)
 
   // Author
   const bookAuthor = document.createElement('p');
-  bookAuthor.className = 'book-author'
-  bookAuthor.textContent = myLibrary[i].author;
+  bookAuthor.className = 'book-author';
+  bookAuthor.textContent = myLibrary[myLibrary.length -1].author;
   bookContainer.appendChild(bookAuthor);
 
-  // Completion
-  const bookCompletion = document.createElement('p');
-  bookCompletion.textContent = myLibrary[i].completion;
-  bookContainer.appendChild(bookCompletion);
+  // Pages
+  const bookPages = document.createElement('p');
+  bookPages.className = 'book-pages';
+  bookPages.textContent = myLibrary[myLibrary.length -1].pages;
+  bookPages.textContent += " pages"
+  bookContainer.appendChild(bookPages);
 
-  // Append bookContainer to bookList div in body
-  let bookList = document.querySelector('.book-list');
-  bookList.appendChild(bookContainer);
+  // Read?
+  const bookRead = document.createElement('p');
+  bookRead.className = 'book-read'
+  bookRead.textContent = myLibrary[myLibrary.length -1].read ? "Read" : "Not Read"
+  bookContainer.appendChild(bookRead)
+
+  const bookList = document.querySelector('.book-list')
+  bookList.appendChild(bookContainer)
 }
 
+// Change read status
+
+
+// Modal Settings
 const modal = document.querySelector('.modal');
 const openModal = document.querySelector('.book-btn');
-const closeModal = document.querySelector('.something');
+const closeModal = document.querySelector('.close-modal');
 
 openModal.addEventListener('click', () => {
   modal.showModal();
-  console.log('great success')
+})
+
+closeModal.addEventListener('click', () => {
+  modal.close();
+})
+
+
+// Apply form submission to add book to library
+const form = document.querySelector('.form');
+form.addEventListener('submit', () => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('is-read').checked;
+
+  addBookToLibrary(title, author, pages, read)
 })
